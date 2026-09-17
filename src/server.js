29360dec92e41ea1ae529fe20ea48d1c5ea871f4
skorohodov97 <skorohodovdanil07@gmail.com>
2026-09-17 15:22:28 +0300

    feat: add environment configuration

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import app from './app.js';
+import config from './config/index.js';
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}`);
 });
