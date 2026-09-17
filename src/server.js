e472769344ef592cd8c230bc4acae4faa5a69cbb
skorohodov97 <skorohodovdanil07@gmail.com>
2026-09-17 14:17:47 +0300

    feat: add health endpoint

--- src/server.js
+++ src/server.js
@@ -1,1 +1,7 @@
-console.log("Server is running on port 3000");
+import app from './app.js';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
