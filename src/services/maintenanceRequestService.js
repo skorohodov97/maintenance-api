61365c32ee84f94c4a87a985edb7a9f7c04b9c72
skorohodov97 <skorohodovdanil07@gmail.com>
2026-09-21 15:09:48 +0300

    feat: add maintenance request service

---
+++ src/services/maintenanceRequestService.js
@@ -0,0 +1,53 @@
+import equipmentRepository from '../repositories/equipmentRepository.js';
+import maintenanceRequestRepository from '../repositories/maintenanceRequestRepository.js';
+
+const ensureEquipmentExists = async (equipmentId) => {
+  const equipment = await equipmentRepository.findById(equipmentId);
+
+  if (!equipment) {
+    const error = new Error('Equipment not found');
+    error.code = 'EQUIPMENT_NOT_FOUND';
+
+    throw error;
+  }
+};
+
+const maintenanceRequestService = {
+  async getAllRequests() {
+    return maintenanceRequestRepository.findAll();
+  },
+
+  async getRequestById(id) {
+    return maintenanceRequestRepository.findById(id);
+  },
+
+  async getRequestsByEquipmentId(equipmentId) {
+    return maintenanceRequestRepository.findByEquipmentId(equipmentId);
+  },
+
+  async createRequest(data) {
+    await ensureEquipmentExists(data.equipmentId);
+
+    return maintenanceRequestRepository.create(data);
+  },
+
+  async updateRequest(id, data) {
+    const request = await maintenanceRequestRepository.findById(id);
+
+    if (!request) {
+      return null;
+    }
+
+    if (data.equipmentId !== undefined) {
+      await ensureEquipmentExists(data.equipmentId);
+    }
+
+    return maintenanceRequestRepository.update(id, data);
+  },
+
+  async deleteRequest(id) {
+    return maintenanceRequestRepository.delete(id);
+  },
+};
+
+export default maintenanceRequestService;
